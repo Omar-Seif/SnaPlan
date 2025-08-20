2bea4a03adf7cd455fe9cbfb34faf0be39afc343
AmrElShamyDEV <rocketengineer1234@gmail.com>
2025-08-20 15:35:23 +0300

    Added room feature to venue form (without onsubmit handling)

--- frontend/src/pages/Organizer/CreateEvent.tsx
+++ frontend/src/pages/Organizer/CreateEvent.tsx
@@ -1,5 +1,4 @@
 import EventForm from "../../components/EventForm";
-import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 
 const CreateEventOrganizer = () => {
