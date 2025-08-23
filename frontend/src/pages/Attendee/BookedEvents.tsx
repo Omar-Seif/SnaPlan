c4a71d1c2788f7c0dcdce15c9b424ee03a2e7234
Omar-Seif <162040202+Omar-Seif@users.noreply.github.com>
2025-08-23 04:06:12 +0300

    cpmmtit

--- frontend/src/pages/Attendee/BookedEvents.tsx
+++ frontend/src/pages/Attendee/BookedEvents.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import EventsCard from "../../components/EventsCard"
 import Navbar from "../../components/Navbar"
 import { eventsDummy } from '../../data/events'
-import { getEvents } from "../../services/EventService";
+import { getEventCardsMock } from "../../services/EventService";
 import type { Event } from '../../types/Event';
 
 
@@ -10,17 +10,17 @@ import type { Event } from '../../types/Event';
 
 const BookedAttendee = () => {
 
-    const [eventList, setEventList] = useState<Event[]>([]);
+    const [eventCards, setEventCards] = useState<Event[]>([]);
     const [error, setError] = useState<null | string>(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchEvents = async () => {
+        const fetchEventCards = async () => {
             try {
                 setLoading(true);
                 setError(null);
-                const events = await getEvents();
-                setEventList(events);
+                const events = await getEventCardsMock();
+                setEventCards(events);
             } catch (err) {
                 console.error(err);
                 setError(err instanceof Error ? err.message : "Failed to load events");
@@ -29,7 +29,7 @@ const BookedAttendee = () => {
             }
         };
 
-        fetchEvents();
+        fetchEventCards();
     }, []);
 
     return (
@@ -44,22 +44,23 @@ const BookedAttendee = () => {
 
             <div className="container mx-auto px-4 py-8">
 
-                {/* {error && <div className="error-message">{error}</div>}
+                {error && <div className="error-message">{error}</div>}
 
                 {loading ? <div>Loading</div> :
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
 
-                        {eventList.map((event, index) => (
+                        {eventCards.map((event, index) => (
                             <EventsCard event={event} key={index} />
                         ))}
-                    </div>} */}
+                    </div>}
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
 
                     {eventsDummy.map((event, index) => (
                         <EventsCard event={event} key={index} />
                     ))}
-                </div>
+                </div> */}
+
             </div>
 
         </>
--- frontend/src/services/EventService.ts
+++ frontend/src/services/EventService.ts
@@ -1,10 +1,12 @@
 // src/services/eventService.ts
 import { API_BASE_URL } from '../config';
-import type { Event } from '../types/Event';
+import { draftEvents } from '../data/draftevents';
+import { eventsDummy } from '../data/events';
+import type { DraftEvent, Event } from '../types/Event';
 
 
-
-export const getEvents = async (): Promise<Event[]> => {
+//get EventCards Real
+export const getEventCards = async (): Promise<Event[]> => {
     const response = await fetch(`${API_BASE_URL}/api/Events/simple`);
 
     if (!response.ok) {
@@ -14,3 +16,35 @@ export const getEvents = async (): Promise<Event[]> => {
 
 };
 
+
+
+// get EventCards Mock
+export const getEventCardsMock = async (): Promise<Event[]> => {
+    // Simulate network delay
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    // Return the dummy data as if it came from an API
+    return Promise.resolve(eventsDummy as unknown as Event[]);
+};
+
+// DraftEvents
+
+// get DraftEvents Real
+// export const getDraftEvents = async (): Promise<DraftEvent[]> => {
+//     const response = await fetch(`${API_BASE_URL}/api/Events/simple`);
+
+//     if (!response.ok) {
+//         throw new Error('Failed to fetch events');
+//     }
+//     return response.json();
+// };
+
+
+// get DraftEvents Mock
+export const getDraftEventsMock = async (): Promise<DraftEvent[]> => {
+    // Simulate network delay
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    // Return the dummy data as if it came from an API
+    return Promise.resolve(draftEvents as unknown as DraftEvent[]);
+};
